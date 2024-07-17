bec703ba341fa37efcb479a9107bb94d6ba2f3a6
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-07-17 17:05:15 +0200

    Feat: Crated Props AppCard

---
+++ src/components/AppCard.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+    props: {
+        cardObj: Object,
+    },
+    data() {
+        return {
+            baseSrc: "http://127.0.0.1:8000/storage" 
+        }
+    }
+}
+</script>
+
+<template>
+           
+        <div class="card ">
+
+            <!-- image -->
+            <img class="card-img-top w-100" :src="`${baseSrc}/${cardObj.image}`" alt="Card image cap">
+            <!-- /image -->
+
+            <!-- card text -->
+            <div class="card-body d-flex flex-column p-0 ps-2 mt-2">
+
+                <!-- name -->
+                <span class="card-title p-0 m-0 cutText">
+                    <strong>
+                        {{ cardObj.name }}
+                    </strong>
+                </span>
+
+                <!-- address -->
+                <span class="card-title p-0 m-0 cutText">{{ cardObj.address }}</span>
+
+
+                <!-- types -->
+                <ul v-if="cardObj.types && cardObj.types.length > 0" class="list-unstyled">
+                    <li v-for="(type, index) in cardObj.types" :key="index">
+                        <strong>
+                            Cucina:
+                        </strong>
+                        {{ type.name }}
+                    </li>
+                </ul>
+                <!-- types -->
+
+            </div>
+            <!-- /card text -->
+
+        </div>
+        
+    
+</template>
+
+<style scoped>
+.main-content {
+  padding: 20px 0;
+}
+
+.form-check-inline {
+  margin: 10px;
+}
+
+.card {
+  height: 100%;
+}
+
+.card-img-top {
+  width: 100%;
+  height: auto;
+}
+</style>
--- src/pages/AppHomePage.vue
+++ src/pages/AppHomePage.vue
@@ -1,29 +1,29 @@
 <script>
 import axios from 'axios'
+import AppCard from '../components/AppCard.vue'
 
 export default {
+  components: {
+    AppCard
+  },
   data() {
     return {
       restaurantsList: [],
+      typesList: [],
       selectedTypes: [],
-      types: [],
       baseSrc: "http://127.0.0.1:8000/storage"
     }
   },
   created() {
-    const typeArray = [1, 2];
     this.CallRestaurant()
     this.CallTypes()
   },
-
   methods: {
     CallRestaurant() {
-      console.log('ciao');
-      const encodedTypes = encodeURIComponent(JSON.stringify(this.selectedTypes)); // Codifica l'array in una stringa URL
-      axios.get(`http://127.0.0.1:8000/api/restaurants?type=${encodedTypes}`)
+      const selectedTypesJson = encodeURIComponent(JSON.stringify(this.selectedTypes)); // Codifica l'array in una stringa URL
+      axios.get(`http://127.0.0.1:8000/api/restaurants?type=${selectedTypesJson}`)
         .then(response => {
           this.restaurantsList = response.data.result;
-          console.log(this.restaurantsList);
         })
         .catch(error => {
           console.error('Errore durante la chiamata API:', error.message || JSON.stringify(error));
@@ -36,8 +36,7 @@ export default {
       })
         .then(response => {
 
-          this.types = response.data.result;
-          console.log(this.types);
+          this.typesList = response.data.result;
         })
         .catch(error => {
           console.error('Errore durante la chiamata API:', error);
@@ -56,13 +55,6 @@ export default {
       }
       this.CallRestaurant();
     },
-
-// modifica
-    goToShowPage(slug) {
-      this.$router.push({ name: 'show-restaurant', params: { slug } });
-    }
-// modifica
-
   }
 }
 </script>
@@ -72,7 +64,7 @@ export default {
   <!-- checkbox types -->
   <div class="container w-50 mt-5">
     <div class="row justify-content-center align-items-center">
-      <div v-for="curType in types" :key="curType.id"
+      <div v-for="curType in typesList" :key="curType.id"
         class="col-lg-4 col-md-6 col-sm-6 col-xs-6 col-6 mb-2 gap-2 btn-group btn-group-toggle">
         <input type="checkbox" class="btn-check" :id="'type-' + curType.id" name="types" :value="curType.id"
           @change="(event) => { SelectType(event.target.value, event.target.checked); }">
@@ -88,39 +80,9 @@ export default {
 
       <!-- card -->
       <div v-for="curRestaurant in restaurantsList" :key="curRestaurant.id"
-        class="card col-lg-3 col-md-4 col-sm-5 col-7 p-0" @click="goToShowPage(curRestaurant.slug)"> <!--evento di click sulla card-->
+        class="card col-lg-3 col-md-4 col-sm-5 col-7 p-0">
 
-        <!-- image -->
-        <img class="card-img-top w-100" :src="`${baseSrc}/${curRestaurant.image}`" alt="Card image cap">
-        <!-- /image -->
-
-        <!-- card text -->
-        <div class="card-body d-flex flex-column p-0 ps-2 mt-2">
-
-          <!-- name -->
-          <span class="card-title p-0 m-0 cutText">
-            <strong>
-              {{ curRestaurant.name }}
-            </strong>
-          </span>
-
-          <!-- address -->
-          <span class="card-title p-0 m-0 cutText">{{ curRestaurant.address }}</span>
-
-
-          <!-- types -->
-          <ul v-if="curRestaurant.types && curRestaurant.types.length > 0" class="list-unstyled">
-            <li v-for="(type, index) in curRestaurant.types" :key="index">
-              <strong>
-                Cucina:
-              </strong>
-              {{ type.name }}
-            </li>
-          </ul>
-          <!-- types -->
-
-        </div>
-        <!-- /card text -->
+       <AppCard :cardObj="curRestaurant" />
 
       </div>
       <!-- /card -->
@@ -135,8 +97,6 @@ export default {
   </div>
 
 </template>
-
-
 
 <style scoped>
 .main-content {
