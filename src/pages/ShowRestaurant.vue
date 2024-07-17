8de98a14c6fd80a43e3bdcad896c92c72deb2604
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-07-17 18:57:48 +0200

    Feat: fix style showpage restaurant

--- src/pages/ShowRestaurant.vue
+++ src/pages/ShowRestaurant.vue
@@ -28,20 +28,28 @@ export default {
 
 <template>
 
-    <div class="container mt-5" v-if="restaurant">
-        <h1>{{ restaurant.name }}</h1>
-        <img :src="`${baseSrc}/${restaurant.image}`" alt="Restaurant image">
-        <p>{{ restaurant.description }}</p>
-        <p>{{ restaurant.address }}</p>
-        <ul v-if="restaurant.types && restaurant.types.length > 0">
-            <li v-for="(type, index) in restaurant.types" :key="index">
-                {{ type.name }}
-            </li>
-        </ul>
+    <div class="container mt-5 " v-if="restaurant">
+          <div class="row">
+            <div class="col-6 ">
+                <img :src="`${baseSrc}/${restaurant.image}`" class="w-75" alt="Restaurant image">
+            </div>
+            <div class="col-6 py-3">                
+                    <h2> Nome: {{ restaurant.name }}</h2>
+                    <h2> Indirizzo: 
+                        <h4>{{ restaurant.address }}</h4>
+                    </h2>
+                    <h2>Descrizione:</h2>
+                    <h4>{{ restaurant.description }}</h4>  
+                    <h2>Tipologia:</h2>  
+                    <ul v-if="restaurant.types && restaurant.types.length > 0">
+                        <li class="list-group-item" v-for="(type, index) in restaurant.types" :key="index">
+                           <h5>{{ type.name }}</h5> 
+                        </li>
+                    </ul>
+            </div>
+          </div>
     </div>
-    <div v-else>
-        <p>Loading...</p>
-    </div>
+    
 
 </template>
 
