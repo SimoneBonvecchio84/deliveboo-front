56e85f86e1044ad6f3b60978c24f8a8925c23fd7
WalterCorsini <corsini.walter@gmail.com>
2024-07-18 14:36:37 +0200

    fix header and logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,60 +8,62 @@ export default {
   <header>
     <!-- Gestione NavBar -->
     <nav class="navbar navbar-expand-lg ">
-      <div class="container ps-5 pe-5 ">
+      <div class="container ps-5 pe-5 d-flex justify-content-between">
         <!-- logo -->
-        <div class="navbar-brand d-flex justify-content-between">
+        <!-- d-flex justify-content-between -->
+        <div class="navbar-brand">
           <img
             src="../assets/img/logo_top.png"
             alt="Logo"
-            class="d-inline-block align-text-top"
+            class="d-inline-block img-fluid align-text-top"
           />
+          <!--  -->
         </div>
         <!-- /logo -->
          <!-- Hamburger menu -->
-        <button
-          class="navbar-toggler collapsed bg-darkred"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon text-white"></span>
-        </button>
-         <!-- /Hamburger menu -->
-        <div
-          class="navbar-collapse collapse"
-          id="navbarSupportedContent"
-          style=""
-        >
-          <!-- Lato sinistro Navbar -->
-          <ul class="navbar-nav me-auto"></ul>
-          <!-- /Lato sinistro Navbar -->
-
-          <!-- Lato destro Navbar -->
-          <ul class="navbar-nav ml-auto">
-             <!-- /Lato destro Navbar -->
-
-            <!-- Link Di Registrazione  -->
-            <li class="nav-item">
-              <router-link :to = "{ name: 'home'  }" class="nav-link text-light fs-5" href="http://127.0.0.1:8000/login">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-light fs-5" href="http://127.0.0.1:8000/login">Login</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-light fs-5" href="http://127.0.0.1:8000/register"
-                >Register</a
-              >
-            </li>
-            <!-- /Link Di Registrazione  -->
-
-          </ul>
-
-        </div>
-        <!-- /hamburger menu -->
+          <div>
+            <button
+              class="navbar-toggler collapsed bg-red"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+              <i class="fa-solid fa-bars text-white"></i>
+              <!-- <span class="navbar-toggler-icon text-white"></span> -->
+            </button>
+             <!-- /Hamburger menu -->
+            <div
+              class="navbar-collapse collapse"
+              id="navbarSupportedContent">
+              <!-- Lato sinistro Navbar -->
+              <!-- <ul class="navbar-nav me-auto"></ul> -->
+              <!-- /Lato sinistro Navbar -->
+    
+              <!-- Lato destro Navbar -->
+              <ul class="navbar-nav">
+                 <!-- /Lato destro Navbar -->
+    
+                <!-- Link Di Registrazione  -->
+                <li class="nav-item">
+                  <router-link :to = "{ name: 'home'  }" class="nav-link text-light fs-5" href="http://127.0.0.1:8000/login">Home</router-link>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-light fs-5" href="http://127.0.0.1:8000/login">Login</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-light fs-5" href="http://127.0.0.1:8000/register"
+                    >Register</a
+                  >
+                </li>
+                <!-- /Link Di Registrazione  -->
+    
+              </ul>
+    
+            </div>
+            <!-- /hamburger menu -->
+          </div>
       </div>
     </nav>
      <!-- /Gestione NavBar -->
@@ -69,6 +71,8 @@ export default {
 </template>
 
 <style scoped lang="scss">
+
+
 /* Gestione Navbar e Logo  */
 .navbar-brand{
   height: 100px;
@@ -81,7 +85,22 @@ export default {
 .navbar-expand-lg{
   background-color: #ff0000; 
   color: white;
+  height: 100px;
+  width: 100%;
 }
 /* /Colore dell'header  */
 
+.navbar-toggler{
+  border: 2px solid white;
+  outline-width: 0;
+  --bs-navbar-toggler-focus-width: 0;
+}
+
+.navbar-nav.ml-auto {
+    margin-left: auto !important;
+}
+
+.navbar-expand-lg img{
+  width: 15vw;
+}
 </style>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@fortawesome/fontawesome-free': '/node_modules/@fortawesome/fontawesome-free/css/all.min.css',
+    },
+  },
 })
