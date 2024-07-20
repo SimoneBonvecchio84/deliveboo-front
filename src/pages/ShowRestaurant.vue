2df0750353e6e192aa26b8f151f725afdb555b74
WalterCorsini <corsini.walter@gmail.com>
2024-07-20 09:18:54 +0200

    fix show restaurant

--- src/pages/ShowRestaurant.vue
+++ src/pages/ShowRestaurant.vue
@@ -6,15 +6,14 @@ export default {
         return {
             restaurant: [],
             baseSrc: "http://127.0.0.1:8000/storage"
-            
         };
     },
     created() {
         this.fetchRestaurant();
-        
-        
-        
-       
+
+
+
+
     },
     methods: {
         fetchRestaurant() {
@@ -23,7 +22,7 @@ export default {
                 .then(response => {
                     this.restaurant = response.data.result;
                     console.log(this.restaurant);
-                        
+
                 })
                 .catch(error => {
                     console.error('Errore durante la chiamata API:', error.message || JSON.stringify(error));
@@ -35,109 +34,142 @@ export default {
 
 <template>
     <div class="md_container-title">
-        <!-- titolo ristorante -->
+        <!-- title -->
         <h1 class="text-center">{{ restaurant.name }}</h1>
-        <!-- titolo ristorante -->
+        <!-- title -->
     </div>
 
-    <!-- contenitore hero -->
+    <!-- restaurant details -->
     <div class="container mt-5 border-bottom" v-if="restaurant">
         <div class="row">
-            <!-- colonna immagine -->
+            <!-- restaurant image -->
             <div class="col-6 d-flex align-items-center justify-content-end">
                 <div class="w-75 p-2 d-flex justify-content-end">
                     <img :src="`${baseSrc}/${restaurant.image}`" class="w-50" alt="Restaurant image">
                 </div>
             </div>
-            <!-- /colonna immagine -->
+            <!-- /restaurant image -->
 
-            <!-- colonna descrizione -->
+
+            <!-- restaurant description -->
             <div class="col-6 py-3">
                 <div class="p-2">
-                    
-                    <dt>Descrizione</dt>
-                    
-                    <dd>{{ restaurant.description }}</dd>
-                    
-                    <dt>Indirizzo</dt>
-                    <dd>{{ restaurant.address }}
+
+                    <dt>
+                        Descrizione
+                    </dt>
+                    <dd>
+                        {{ restaurant.description }}
                     </dd>
+
+                    <dt>
+                        Indirizzo
+                    </dt>
+                    <dd>
+                        {{ restaurant.address }}
+                    </dd>
+
                     <dt>Cucina</dt>
                     <dd>
                         <ul>
-                            <li  v-for="(type, index) in restaurant.types" :key="index">
+                            <li v-for="(type, index) in restaurant.types" :key="index">
                                 {{ type.name }}
                             </li>
                         </ul>
                     </dd>
                 </div>
             </div>
-            <!-- /colonna descrizione -->
-        
+            <!-- /restaurant description -->
+
+
         </div>
     </div>
-    <!-- /contenitore hero -->
+    <!-- restaurant details -->
 
-    <!-- contenitore piatti -->
-     <div class="container mt-5 ">
-        
-        <div class="row">
-            
-            <!-- colonna piatto -->
-            <h2 class="text-center py-3">Menù</h2>
-            <div class="col-12" v-for="curDish in restaurant.dishes">  
-                
-                <div class="container py-3 d-flex justify-content-center" v-if="restaurant.dishes.length > 0">
-                    <div class="row border-bottom  align-items-center w-50">
-                        <!-- colonna immagine piatto -->
-                        <div class="col-6 py-3 d-flex justify-content-end">
-                            <img :src="`${baseSrc}/${curDish.image}`" class="w-75 rounded-circle" alt="immagine piatto">    
-                        </div>
-                        <!-- /colonna immagine piatto -->
+    <!-- dishes container -->
+    <div class="container-fluid mt-5 ">
 
-                        <!-- collonna descrizione piatto -->
-                        <div class="col-6 py-4">
-                            <div>
-                                <dt>Nome Piatto</dt>
-                                <dd>{{ curDish.name }}</dd>
-                                <dt>Descrizione</dt>
-                                <dd>{{ curDish.description }}</dd>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div>
-                                        <dt>Prezzo</dt>
-                                        <dd>{{ curDish.price }}  &euro;</dd>
-                                        
-                                    </div>
-                                    <router-link :to="{ name: 'cartshopping', params: { dishid: curDish.id, restaurant: restaurant.id, amount: 1 }}">
-                                        <a class="btn  btn-success  py-1 m-0" href="">+</a>
-                                    </router-link>
-                                    
-                                </div>
-                                
-                                <dd>
-                                    <span :class="{'text-success' : curDish.visibility === 1, 'text-danger' : curDish.visibility === 0 }"> <strong>Disponibile</strong></span>
-                                </dd>
-                            </div>
-                        </div>
-                        <!-- /collonna descrizione piatto -->
+        <!-- title menu -->
+        <h2 class="text-center py-3">Menù</h2>
+        <!-- /title menu -->
+
+        <!-- dish card -->
+        <div v-for="curDish in restaurant.dishes">
+
+            <div v-if="restaurant.dishes.length > 0" class="container d-flex justify-content-center" >
+                <div class="row border-bottom  align-items-center w-75">
+                    <!-- image -->
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 py-3 d-flex justify-content-end">
+                        <img :src="`${baseSrc}/${curDish.image}`" class="w-75 rounded-circle" alt="immagine piatto">
                     </div>
-                </div>
-                <div class="container border rounded" v-else>
-                    <h2 class="text-center">Non ci sono ancora piatti!</h2>
-                </div>
-            
-            </div>
-            <!-- /colonna piatto -->
-        
-        </div>
+                    <!-- /image -->
 
-     </div>
-    <!-- /contenitore piatti -->
+                    <!-- description -->
+                    <div class="col-xl-8 col-lg-8 col-md-6 col-sm-12 py-4">
+                        <!-- name -->
+                        <dt>
+                            Nome Piatto
+                        </dt>
+                        <dd>
+                            {{ curDish.name }}
+                        </dd>
+                        <!-- /name -->
+
+                        <!-- description -->
+                        <dt>
+                            Descrizione
+                        </dt>
+                        <dd>
+                            {{ curDish.description }}
+                        </dd>
+                        <!-- /description -->
+
+                        <!-- price and btn shop -->
+                        <div class="d-flex justify-content-between align-items-center">
+
+                            <!-- price -->
+                            <div>
+                                <dt>Prezzo</dt>
+                                <dd>{{ curDish.price }} &euro;</dd>
+                            </div>
+                            <!-- /price -->
+
+                            <!-- btn shop -->
+                            <router-link
+                                :to="{ name: 'cartshopping', params: { dishid: curDish.id, restaurant: restaurant.id, amount: 1 } }">
+                                <a class="btn  btn-success  py-1 m-0" href="">+</a>
+                            </router-link>
+                            <!-- /btn shop -->
+
+                        </div>
+                        <!-- /price and btn shop -->
+
+                        <!-- visibility -->
+                        <dd>
+                            <span
+                                :class="{ 'text-success': curDish.visibility === 1, 'text-danger': curDish.visibility === 0 }">
+                                <strong>Disponibile</strong></span>
+                        </dd>
+                        <!-- visibility -->
+
+                    </div>
+                    <!-- /description -->
+                </div>
+            </div>
+            <div v-else class="container border rounded">
+                <h2 class="text-center">Non ci sono ancora piatti!</h2>
+            </div>
+            
+        </div>
+        <!-- /dish card -->
+
+    </div>
+    <!-- /dishes container -->
 
 </template>
 
 
-<style  lang="scss" scoped>
+<style lang="scss" scoped>
 .md_container-title {
     margin-top: 150px;
 }
