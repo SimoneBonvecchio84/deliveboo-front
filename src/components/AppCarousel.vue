f0c84d7442bb6f41c6086a5ac18c1af874d39c6c
WalterCorsini <corsini.walter@gmail.com>
2024-07-22 08:59:39 +0200

    add  comment carousel

--- src/components/AppCarousel.vue
+++ src/components/AppCarousel.vue
@@ -21,11 +21,13 @@ export default {
     },
 
     methods: {
+        // carousel slide show
         autoPlay() {
             this.interval = setInterval(() => {
                 this.currentFood = (this.currentFood + 1) % this.foods.length;
             }, 4000);
         },
+        // for dynamic image
         getImage(name) {
             if (this.flag.includes(name)) {
                 return new URL(`../assets/img/${name}.png`, import.meta.url).href;
@@ -40,29 +42,57 @@ export default {
 <template>
     <div class="container d-flex align-items-center ms_container">
         <div class="row" name="slide" mode="out-in">
+
+            <!-- dynamic image -->
             <div class="col-12 col-md-6 col-lg-5 d-flex justify-content-center p-0 m-0">
                 <transition name="fade" mode="out-in">
-                    <div :key="currentFood" :class="{ 'active-slide': true }" class="container-img-food ms_carousel-slide">
+                    <div :key="currentFood" :class="{ 'active-slide': true }"
+                        class="container-img-food ms_carousel-img">
                         <img :src="foods[currentFood].image" alt="" class="ms_food-img img img-fluid" />
                     </div>
                 </transition>
             </div>
-            <div class="col-12 col-md-6 col-lg-7 ms_text container-text-hero">
+            <!-- /dynamic image -->
+
+            <!-- dynamic text -->
+            <div class="col-12 col-md-6 col-lg-7 ms_text ms_carousel-text">
                 <transition name="slide-fade" mode="out-in">
                     <!-- <p :key="currentFood" class="ms_food-text py-4">{{ foods[currentFood].text }}</p> -->
-                    <img :src="foods[currentFood].text" alt="" :key="currentFood" class="ms_food-img img img-fluid w-50" />
+                    <img :src="foods[currentFood].text" alt="" :key="currentFood"
+                        class="ms_food-img img img-fluid w-50" />
                 </transition>
             </div>
+            <!-- dynamic text -->
+
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+
 .ms_container {
     width: 80%;
+
+    // img container
+    .container-img-food {
+        padding: 0;
+        margin: 0;
+
+        img {
+            width: 100%;
+        }
+    }
+
+    .ms_carousel-text {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        /* Posizionamento relativo per permettere la transizione */
+    }
 }
 
-.ms_carousel-slide {
+.ms_carousel-img {
     display: flex;
     align-items: center;
     justify-content: center;
@@ -70,25 +100,21 @@ export default {
     transform: translateX(0);
     transition: transform 1s ease;
     position: relative;
-
-    // .ms_food-img {
-    //     width: 400px;
-    // }
 }
 
-.ms_text {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative; /* Posizionamento relativo per permettere la transizione */
-}
+// /img container
 
-.ms_food-text {
-    font-style: italic;
-    font-size: 1.5rem;
-    text-align: center;
+// text container
+.container-text-hero {
+    padding: 0;
+    margin: 0;
 }
+// /text container
 
+
+// animation carousel
+
+// animation img food
 .fade-enter-active {
     transition: opacity 3s ease;
 }
@@ -97,14 +123,19 @@ export default {
     transition: none;
 }
 
-.fade-enter-from, .fade-leave-to {
+.fade-enter-from,
+.fade-leave-to {
     opacity: 0;
 }
 
-.slide-fade-enter-active{
+// /animation img food
+
+// animation img text
+.slide-fade-enter-active {
     transition: all 1.5s ease;
 }
-.slide-fade-leave-active{
+
+.slide-fade-leave-active {
     transition: all 6.5 ease;
 }
 
@@ -114,20 +145,8 @@ export default {
 }
 
 .slide-fade-leave-to {
-    opacity: 1; /* Mantieni opacità a 1 per testo uscente */
-    transform: translateX(0); /* Mantieni posizione originale per testo uscente */
+    opacity: 1;
+    transform: translateX(0);
 }
 
-.container-img-food{
-    padding: 0;
-    margin: 0;
-    img{
-        width: 100%;
-    }
-}
-
-.container-text-hero{
-    padding: 0;
-    margin: 0;
-}
-</style>
+// /animation img text</style>
