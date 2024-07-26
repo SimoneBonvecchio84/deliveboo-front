50b853fb1a4e3474b9edb9b8ea0e2f2853317590
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-07-26 19:40:45 +0200

    Fix: custom style in AppNotFound

--- src/pages/AppNotFound.vue
+++ src/pages/AppNotFound.vue
@@ -2,29 +2,39 @@
 </script>
 
 <template>
-    <div class="form-containert text-center md_container d-flex flex-column align-items-center justify-content-center ">
-        <i class="fas fa-exclamation-triangle py-5"></i>
-        <h1 class="border border-danger rounded p-5">Errore! Pagina non trovata</h1>
-        <router-link :to="{ name: 'home' }" >
-            <button class="btn btn-primary" >Torna Alla Home</button>
-        </router-link>
+
+    <div class="containert md_container">
+
+
+        <div class="text-center ">
+            <div>
+                <img src="../assets/img/notFoundPage.webp" alt="">
+            </div>
+            
+            <h2 class=" py-2 mb-4">Ops...Qualcosa è andato storto!</h2>
+            <router-link :to="{ name: 'home' }">
+                
+                <button class="btn btn-primary">Torna Alla Home</button>
+            </router-link>
+
+        </div>
     </div>
+
+
+
+
 </template>
 
 
 <style lang="scss" scoped>
 @use "../sass/colorpalette.scss" as *;
-    .md_container {
-        
-        .fas {
-            font-size: 7rem;
-            color: $red;
-            margin-top: 100px;            
-        }
 
-        button {
-            margin-top: 50px;
-            margin-bottom: 100px;
-        }
-    }
+.md_container {
+    min-height: 90vh;
+    margin-top: 130px;
+}
+
+.fas {
+    font-size: 3rem;
+}
 </style>
